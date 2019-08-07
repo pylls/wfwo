2c0e113b3533b8b5ee7008895a9cb1a81dab9e80
pulls <tobias.pulls@kau.se>
2019-08-07 12:04:43 +0200

    small changes for python3 support

--- example.sh
+++ example.sh
@@ -3,12 +3,27 @@
 
 echo "WF type: probabilities for each possible WF label for each classification."
 echo ""
-python sim.py -lm example/df-nodef-test-labels-mon.pkl -lu example/df-nodef-test-labels-unmon.pkl -lp example/df-nodef-predictions.pkl -s example_prob.pkl
+./sim.py -lm example/df-nodef-test-labels-mon.pkl -lu example/df-nodef-test-labels-unmon.pkl -lp example/df-nodef-predictions.pkl -s example_prob_nodef.pkl
 echo ""
-python metrics.py -lm example/df-nodef-test-labels-mon.pkl -lu example/df-nodef-test-labels-unmon.pkl -wf example/df-nodef-predictions.pkl -p example_prob.pkl
+./metrics.py -lm example/df-nodef-test-labels-mon.pkl -lu example/df-nodef-test-labels-unmon.pkl -wf example/df-nodef-predictions.pkl -p example_prob_nodef.pkl -o example_nodef
 
 echo "WF type: single prediction (label) for each classification."
 echo ""
-python sim.py -lm example/df-nodef-test-labels-mon.pkl -lu example/df-nodef-test-labels-unmon.pkl -lp example/df-nodef-predictions-single.pkl -s example_single.pkl
+./sim.py -lm example/df-nodef-test-labels-mon.pkl -lu example/df-nodef-test-labels-unmon.pkl -lp example/df-nodef-predictions-single.pkl -s example_prob_single.pkl
 echo ""
-python metrics.py -lm example/df-nodef-test-labels-mon.pkl -lu example/df-nodef-test-labels-unmon.pkl -wf example/df-nodef-predictions-single.pkl -p example_single.pkl
+./metrics.py -lm example/df-nodef-test-labels-mon.pkl -lu example/df-nodef-test-labels-unmon.pkl -wf example/df-nodef-predictions-single.pkl -p example_prob_single.pkl
+
+# Below you find lines to run WF+WO sim and metrics against the WTF-PAD and
+# Walkie-Talkie examples also included in examples.zip
+
+echo "Probabilities, as above, but for WTF-PAD defense"
+echo ""
+./sim.py -lm example/df-wtfpad-test-labels-mon.pkl -lu example/df-wtfpad-test-labels-unmon.pkl -lp example/df-wtfpad-predictions.pkl -s example_prob_wtfpad.pkl
+echo ""
+./metrics.py -lm example/df-wtfpad-test-labels-mon.pkl -lu example/df-wtfpad-test-labels-unmon.pkl -wf example/df-wtfpad-predictions.pkl -p example_prob_wtfpad.pkl -o example_wtfpad
+
+echo "Probabilities, as above, but for Walkie-Talkie defense"
+echo ""
+./sim.py -lm example/df-wt-test-labels-mon.pkl -lu example/df-wt-test-labels-unmon.pkl -lp example/df-wt-predictions.pkl -s example_prob_wt.pkl
+echo ""
+./metrics.py -lm example/df-wt-test-labels-mon.pkl -lu example/df-wt-test-labels-unmon.pkl -wf example/df-wt-predictions.pkl -p example_prob_wt.pkl -o example_wt
